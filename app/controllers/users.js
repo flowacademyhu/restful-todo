2fe96903e46d2eb2236f2e3dde554ed4efeb41c8
g4bor <gabor.tanacs@yahoo.com>
2018-08-28 15:20:35 +0200

    add server.js, users controller

---
+++ app/controllers/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const users = express.Router({mergeParams: true});
+const models = require('../models');
+const User = models.User;
+/*
+users.param('userId', (req, res, next, id) => {
+  User.findById(id).then((userRecord) => {
+    req.user = userRecord;
+    next();
+  });
+});
+*/
+// get all of the users
+users.get('/', (req, res) => {
+  User.findAll().then((allUser) => {
+    res.json(allUser);
+  });
+});
+
+// get a specific user
+users.get('/userId', (req, res) => {
+  res.json(req.user);
+});
+
+// delete a specific user
+users.delete('/userId', (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = users;
