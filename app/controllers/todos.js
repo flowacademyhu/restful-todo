1d377fe81e4fe60f9957ffb4d2507936b387b475
Varga Gabor <vargagaborszeged@gmail.com>
2018-08-28 14:59:46 +0200

    todo app.js

---
+++ app/controllers/todos.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const todos = express.Router();
+const models = require('../models');
+
+// Index
+todos.get('/', (req, res) => {
+  Todo.findAll().then((allTodo) => {
+    res.json(allTodo);
+  });
+});
+
+// New
+todos.get('/new', (req, res) => {
+  res.json(???);
+});
+
+// Show
+todos.get('/:id', (req, res) => {
+  Todo.findById(req.params.id).then((todoRecord) => {
+    res.json(todoRecord);
+  });
+});
+
+// Create
+todos.post('/', (req, res) => {
+  Todo.create({
+    //
+  }).then(todo => {
+    res.json();
+  }).catch(error => {
+    res.json(error);
+  });
+});
+
+// Edit
+todos.get('/:id/edit', (req, res) => {
+  Todo.findById(req.params.id).then((todoRecord) => {
+    res.json(todoRecord);
+  });
+});
+
+// Update
+todos.put('/:id', (req, res) => {
+  Todo.findById(req.params.id).then((todoRecord) => {
+    todoRecord.update(req.body).then((updatedTodoRecord) => {
+      res.json(updatedTodoRecord)
+    });
+  });
+});
