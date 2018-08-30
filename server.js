0a3d994238734f0b9ef223900b8711dc6d9bd2c9
Varga Gabor <vargagaborszeged@gmail.com>
2018-08-30 14:14:40 +0200

    changes

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-// express settings
 const express = require('express');
 const app = express();
-
-// controllers
-// const index = require('./app/controllers/indexes');
-// app.use('/', index);
 
 const users = require('./app/controllers/users');
 app.use('/users', users);
 
-// static folder
-// app.use(express.static('./public'));
+const todos = require('./app/controllers/todos');
+app.use('/todos', todos);
 
-// APP start!
 app.listen(8080);
